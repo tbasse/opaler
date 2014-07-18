0c91b6984e3014922fe8281b07afa55b125ec181
tbasse <thorsten@tgdb.io>
2014-07-18 11:16:28 +1000

    update app.js to use transaction method

--- app.js
+++ app.js
@@ -5,10 +5,21 @@ var OpalCard = require('./opal.js'),
 
 var opal = new OpalCard(config.opal.username, config.opal.password);
 
+
+opal.getCardTransactions(function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+    console.log('');
+  }
+});
+
 opal.getCardInfo(function (err, data) {
   if (err) {
     console.log(err);
   } else {
     console.log(data);
+    console.log('');
   }
 });
