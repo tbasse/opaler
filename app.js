b2635a5db36422b6b25e026f27d78747e2003e69
tbasse <thorsten@tgdb.io>
2014-08-04 12:12:21 +1000

    update app.js example

--- app.js
+++ app.js
@@ -24,6 +24,23 @@ var opal = new OpalCard(config.opal.username, config.opal.password);
 //   }
 // });
 
+function getOverallFares(transactions) {
+  var overallPrice    = 0,
+      overallDiscount = 0,
+      overallPaid     = 0;
+  transactions.forEach(function (transaction) {
+    if (transaction.journey) {
+      overallPrice    += transaction.fare.price;
+      overallDiscount += transaction.fare.discount;
+      overallPaid     += transaction.fare.paid;
+    }
+  });
+  return {
+    overallPrice: overallPrice,
+    overallDiscount: overallDiscount,
+    overallPaid: overallPaid
+  };
+}
 
 opal.getCardInfo()
 .then(function (data) {
@@ -34,10 +51,15 @@ opal.getCardInfo()
 .then(function (data) {
   console.log(data);
   console.log('');
-  return opal.getCardTransactions();
+  return opal.getCardTransactions({
+    month: 7,
+    year: 2014
+  });
 })
 .then(function (data) {
-  console.log(data);
+  console.log(JSON.stringify(data, null, '  '));
+  console.log('');
+  console.log(JSON.stringify(getOverallFares(data), null, '  '));
 })
 .fail(function (err) {
   console.log(err);
