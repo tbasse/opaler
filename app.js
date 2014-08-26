e2392199078f1b020eb64e06e5e7ac3f340d1c46
tbasse <thorsten@tgdb.io>
2014-08-26 15:12:35 +1000

    update app.js to return data from all 4 transaction pages

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var OpalCard = require('./lib/opal.js'),
-    config   = require('./config.json');
+    config   = require('./config.json'),
+    Q        = require('q');
 
 var opal = new OpalCard(config.opal.username, config.opal.password);
 
@@ -23,42 +24,67 @@ var opal = new OpalCard(config.opal.username, config.opal.password);
 // });
 
 function getOverallFares(transactions) {
-  var overallPrice    = 0,
+  var overallJourneys = 0,
+      overallPrice    = 0,
       overallDiscount = 0,
       overallPaid     = 0;
   transactions.forEach(function (transaction) {
     if (transaction.journey) {
+      overallJourneys += 1;
       overallPrice    += transaction.fare.price;
       overallDiscount += transaction.fare.discount;
       overallPaid     += transaction.fare.paid;
     }
   });
   return {
+    overallJourneys: overallJourneys,
     overallPrice: overallPrice,
     overallDiscount: overallDiscount,
     overallPaid: overallPaid
   };
 }
 
-opal.getCardInfo()
+// opal.getCardInfo()
+// .then(function (data) {
+//   console.log(data);
+//   console.log('');
+//   return opal.getUserDetails();
+// })
+// .then(function (data) {
+//   console.log(data);
+//   console.log('');
+//   return opal.getCardTransactions({
+//     pageIndex: 1
+//   });
+// })
+// .then(function (data) {
+//   console.log(JSON.stringify(data, null, '  '));
+//   console.log('');
+//   console.log(JSON.stringify(getOverallFares(data), null, '  '));
+// })
+// .fail(function (err) {
+//   console.log(err);
+// });
+
+var fnArray = [
+  opal.getCardTransactions({pageIndex: 1}),
+  opal.getCardTransactions({pageIndex: 2}),
+  opal.getCardTransactions({pageIndex: 3}),
+  opal.getCardTransactions({pageIndex: 4})
+];
+
+return Q.all(fnArray)
 .then(function (data) {
-  console.log(data);
-  console.log('');
-  return opal.getUserDetails();
-})
-.then(function (data) {
-  console.log(data);
-  console.log('');
-  return opal.getCardTransactions({
-    month: 7,
-    year: 2014
+  return data.reduce(function(a, b) {
+    return a.concat(b);
   });
 })
 .then(function (data) {
-  console.log(JSON.stringify(data, null, '  '));
-  console.log('');
-  console.log(JSON.stringify(getOverallFares(data), null, '  '));
+  console.log(data);
+  var overall = getOverallFares(data);
+  console.log(JSON.stringify(overall, null, '  '));
 })
 .fail(function (err) {
-  console.log(err);
-});
+  console.error(err);
+})
+.done();
