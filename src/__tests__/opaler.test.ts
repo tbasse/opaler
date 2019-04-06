4308e0297acdc42705735d42aa7b548b5eda433a
tbasse <thorsten@tgdb.io>
2019-04-06 12:44:10 +1100

    add more tests for request

--- src/__tests__/opaler.test.ts
+++ src/__tests__/opaler.test.ts
@@ -5,7 +5,8 @@ import * as request from 'request';
 
 jest.mock('request', () => ({
   jar: jest.fn().mockImplementation(() => request.jar),
-  get: jest.fn().mockImplementation(() => null),
+  get: jest.fn().mockImplementation(() => request.get),
+  post: jest.fn().mockImplementation(() => request.post),
 }));
 
 test('#Opaler', () => {
@@ -26,6 +27,60 @@ test('#getAccount()', () => {
   expect(request.get).toBeCalledWith(
     'https://www.opal.com.au/registered/my-details/',
     expect.any(Object),
+    expect.any(Function),
+  );
+});
+
+test('#authorize()', () => {
+  const opaler = new Opaler('user', 'password');
+  const cb = jest.fn();
+  // tslint:disable-next-line no-string-literal
+  opaler['authorize'](cb);
+
+  expect(request.post).toBeCalledWith(
+    'https://www.opal.com.au/login/registeredUserUsernameAndPasswordLogin',
+    {
+      form: {
+        attempt: '',
+        h_password: 'password',
+        h_username: 'user',
+        submit: 'Log in',
+      },
+      jar: request.jar,
+    },
+    expect.any(Function),
+  );
+});
+
+test('#getRequest()', () => {
+  const opaler = new Opaler('user', 'password');
+  // tslint:disable-next-line no-string-literal
+  opaler['getRequest']('https://www.opal.com.au/some/path', () => null);
+
+  expect(request.get).toBeCalledWith(
+    'https://www.opal.com.au/some/path',
+    { jar: request.jar },
+    expect.any(Function),
+  );
+});
+
+test('#getTransactionsSinglePage()', () => {
+  const opaler = new Opaler('user', 'password');
+  // tslint:disable-next-line no-string-literal
+  opaler['getTransactionsSinglePage'](
+    {
+      month: 1,
+      year: 2018,
+      pageIndex: 1,
+      cardIndex: 1,
+      ts: 1234,
+    },
+    () => null,
+  );
+
+  expect(request.get).toBeCalledWith(
+    'https://www.opal.com.au/some/path',
+    { jar: request.jar },
     expect.any(Function),
   );
 });
